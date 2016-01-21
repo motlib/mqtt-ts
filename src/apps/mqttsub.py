5640d72f27ef85606af15fb7ea695db8e2d24ed6
Andreas Schroeder <andreas@a-netz.de>
2016-01-21 20:28:19 +0800

    Added logging to logfile. Reconnect on mqtt error. Receive indication for mqtt display.

--- src/apps/mqttsub.py
+++ src/apps/mqttsub.py
@@ -5,47 +5,85 @@ from apps.baseapp import BaseApp
 
 
 MQTT_BROKER = 'bpi1'
+
 topics = {
-    '/sensors/rpi2/cputemp/temperature': { 'lbl': 'rpi2 CPU Temperature', 'val': 0 },
-    '/sensors/rpi2/BMP180/pressure': { 'lbl': 'Air Pressure', 'val': 0 },
-    '/sensors/rpi2/HTU21D/temperature': { 'lbl': 'Temperature', 'val': 0 },
-    '/sensors/rpi2/HTU21D/relative humidity': { 'lbl': 'Humidity', 'val': 0 },
-    '/sensors/rpi2/TSL2561/luminosity': { 'lbl': 'Luminosity', 'val': 0 },
+    '/sensors/rpi2/cputemp/temperature': { 'lbl': 'rpi2 CPU Temperature'},
+    '/sensors/rpi2/BMP180/pressure': { 'lbl': 'Air Pressure' },
+    '/sensors/rpi2/HTU21D/temperature': { 'lbl': 'Temperature' },
+    '/sensors/rpi2/HTU21D/relative humidity': { 'lbl': 'Humidity' },
+    '/sensors/rpi2/TSL2561/luminosity': { 'lbl': 'Luminosity' },
     }
+
+CONN_ERR_LIMIT = 5
+
 
 class MQTTSubscriberApp(BaseApp):
 
     def __init__(self):
-        mqttclt = paho.Client()
-        mqttclt.on_message = self.on_message
-        # mqttclt.on_publish = on_publish
+        self.mqtt_connect()
 
-        mqttclt.connect(MQTT_BROKER, port=1883, keepalive=60)
+        for t in topics.values():
+            t['updated'] = False
+            t['val'] = 0
+        
+        
+    def mqtt_connect(self):
+        try:
+            self.conn_err = 0
 
-        for t in topics.keys():
-            mqttclt.subscribe(t, 0)
+            self.mqttclt = paho.Client()
+            self.mqttclt.on_message = self.on_message
+            self.mqttclt.on_disconnect = self.on_disconnect
+            
+            self.mqttclt.connect(MQTT_BROKER, port=1883, keepalive=60)
+            
+            for t in topics.keys():
+                self.mqttclt.subscribe(t, 0)
 
-        mqttclt.loop_start()
-
-        self.mqttclt = mqttclt
-        self.temp = 0
+            self.mqttclt.loop_start()
+        except:
+            logging.exception('Failed to extablish connection to MQTT broker.')
 
         
     def on_message(self, mosq, obj, msg):
+        self.conn_err = 0
+        
         try:
             data = json.loads(msg.payload.decode('utf-8'))
             topics[msg.topic]['val'] = data['value']
+            topics[msg.topic]['updated'] = True
         except Exception as e:
-            #print(e)
-            pass
-        
+            msg = "Failed to parse MQTT message in topic '{0}'."
+            logging.warn(msg.format(msg.topic))
 
+        
+    def on_disconnect(client, userdata, rc):
+        if rc != MQTT_ERR_SUCCESS:
+            logging.warning('MQTT client unexpected disconnect. Trying to reconnect.')
+            
+            self.conn_err += 1
+            if self.conn_err < CONN_ERR_LIMIT:
+                self.mqtt_connect()
+            else:
+                # giving up, 
+                pass
+        else:
+            # disconnect was requested by API, do nothing
+            pass
+            
     def update(self):
-        y = 0
-        for t in topics.values():
-            pstr = '{lbl:>30}: {val:4.1f}'.format(
+        for y, t in enumerate(topics.values()):
+
+            if t['updated']:
+                upd = '*'
+                t['updated'] = False
+            else:
+                upd = ' '
+            
+            pstr = '{lbl:>30}: {val:4.1f} {upd}'.format(
                 lbl=t['lbl'],
-                val=t['val'])
+                val=t['val'],
+                upd=upd)
 
             self.wnd.addstr(y, 0, pstr)
 
--- src/datadisp.py
+++ src/datadisp.py
@@ -1,19 +1,38 @@
 from time import sleep
 import curses
+import logging
+import sys
 
 from appreg import init_apps
 from scrman import ScreenManager
 
+
+def setup_logging():
+    logfile = open('datadisp.log', 'a')
+    
+    logging.basicConfig(
+        filename='datadisp.log',
+        filemode='a',
+        level=logging.WARNING)
+
+
 def main(stdscr):
-    scrman = ScreenManager(stdscr)
-
-    init_apps(scrman)
-
-    # endless loop for data display
-    while True:
-        scrman.update()
-                
-        sleep(1)
+    try:
+        setup_logging()
         
+        scrman = ScreenManager(stdscr)
+
+        init_apps(scrman)
+
+        # endless loop for data display
+        while True:
+            scrman.update()
+                
+            sleep(1)
+    except Exception as e:
+        print(e)
+        logging.exception('Main loop failed')
+        sys.exit(1)
+
 if __name__ == '__main__':
     curses.wrapper(main)
