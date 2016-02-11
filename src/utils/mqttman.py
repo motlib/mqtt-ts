951dfc86a0d3dfb31496266b55cfc988d24aa64e
Andreas Schroeder <andreas@a-netz.de>
2016-02-11 13:44:44 +0800

    Added comments to MQTTManager class.

--- src/utils/mqttman.py
+++ src/utils/mqttman.py
@@ -2,9 +2,6 @@ import paho.mqtt.client as paho
 import json
 import logging
 
-from apps.baseapp import BaseApp
-from apps.valuedisp import ValueDisplayApp
-
 
 STAT_DISCONNECT = 'D'
 STAT_TIMEOUT = 'T'
@@ -13,13 +10,32 @@ STAT_VALID = ' '
 
 
 class MQTTManager():
+    '''Class to manage reception of MQTT messages.
+
+    This automatically reconnects to the broker if the connection is
+    lost. Also checks for timeout conditions of the subscribed topics.
+
+    All times (timeout time) are measured based on the call cycle of
+    the tick() function. Usually this can be called once per second.
+
+    '''
+
     inst = None
     
     def get_instance():
+        '''Return the one created instance of the MQTTManager.
+
+        This is similar to a singleton pattern. You create one
+        instance of the manager as usual and then everywhere retrieve
+        that instance by calling the get_instance() function.
+
+        '''
+        
         if MQTTManager.inst == None:
             raise ValueError('Instance has not yet been created!')
         
         return MQTTManager.inst
+    
     
     def __init__(self, broker):
         self.connected = False
@@ -30,6 +46,11 @@ class MQTTManager():
 
         
     def add_topic(self, topic, timeout=0):
+        '''Add a topic to subscribe. Optionally takes the timeout time for
+        this topic.
+
+        '''
+        
         self.topics[topic] = {
             'payload': None,
             'status': 'N',
@@ -42,10 +63,17 @@ class MQTTManager():
 
             
     def set_timeout(self, topic, timeout):
+        '''Sets the timeout time for a topic.'''
+        
         self.topics[topic]['timeout'] = timeout
             
         
     def get_payload(self, topic):
+        '''Get the last received payload for the given topic.
+
+        This also resets the receive status from updated to valid. So
+        get_status needs to be called before calling this function.'''
+        
         tdata = self.topics[topic]
 
         if tdata['status'] == STAT_UPDATED:
@@ -55,10 +83,18 @@ class MQTTManager():
 
     
     def get_status(self, topic):
+        '''Return the receive status of a topic.
+
+        Must be called before calling get_payload.'''
+        
         return self.topics[topic]['status']
 
     
     def tick(self):
+        '''Execute regular maintenance tasks. Reconnects to the broker
+        if the connection is lost. Checks the timeout status of the
+        subscribed topics.'''
+        
         if not self.connected:
             self.mqtt_connect()
 
@@ -101,6 +137,8 @@ class MQTTManager():
 
             
     def on_message(self, mosq, obj, msg):
+        '''Callback function for the mqtt client. Called on message reception.'''
+        
         try:
             topic = self.topics[msg.topic]
             topic['payload'] = msg.payload.decode('utf-8')
@@ -113,18 +151,21 @@ class MQTTManager():
 
             
     def on_connect(self, client, userdata, flags, rc):
+        '''Callback function for the mqtt client. Called when a connection to 
+        the broker is established.'''
+
         self.connected = True
 
         
     def on_disconnect(client, userdata, rc):
+        '''Callback function for the mqtt client. Called when the connection to
+        the broker is closed or lost.'''
+        
         self.connected = False
 
         msg = 'Unexpected MQTT disconnect with reason {0}.'
         logging.warning(msg.format(rc))
 
-        # mark all topics as error
+        # set all topics to error state
         for topic in self.topics.values():
             topic['status'] = STAT_DISCONNECT
-
-
-        
