4964be729610a96a3d0e5903bbaba7787c67ae10
Andreas Schroeder <andreas@a-netz.de>
2016-02-11 08:18:38 +0800

    Changed datadisp to class. Moved mqttman to utils directory.

--- src/datadisp.py
+++ src/datadisp.py
@@ -2,43 +2,61 @@ from time import sleep
 import curses
 import logging
 import sys
+from argparse import ArgumentParser
 
 from appreg import init_apps
 from scrman import ScreenManager
-from mqttman import MQTTManager
+from utils.mqttman import MQTTManager
 
 
 MQTT_BROKER = '192.168.0.21'
 
+class DataDisp():
 
-def setup_logging():
-    logging.basicConfig(
-        filename='datadisp.log',
-        filemode='a',
-        level=logging.WARNING,
-        format='%(asctime)s %(levelname)s: %(message)s')
+    def setup_args(self):
+        parser = ArgumentParser()
+        parser.add_argument(
+            '-l', '--logfile',
+            help='Path to the logfile.',
+            default='datadisp.log')
 
+        self.args = parser.parse_args()
+    
 
-def main(stdscr):
-    try:
-        setup_logging()
+    def setup_logging(self):
+        '''Set up the logging framework.'''
         
-        scrman = ScreenManager(stdscr)
-        mqtt = MQTTManager(MQTT_BROKER)
-        
-        init_apps(scrman, mqtt)
+        logging.basicConfig(
+            filename=self.args.logfile,
+            filemode='a',
+            level=logging.WARNING,
+            format='%(asctime)s %(levelname)s: %(message)s')
 
-        # endless loop for data display
-        while True:
-            mqtt.tick()
-            scrman.update()
-                
-            sleep(1)
-    except Exception as e:
-        print(e)
-        logging.exception('Main loop failed')
-        sys.exit(1)
+
+    def run(self, stdscr):
+        try:
+            self.setup_args()
+            
+            self.setup_logging()
+            
+            self.scrman = ScreenManager(stdscr)
+            self.mqtt = MQTTManager(MQTT_BROKER)
+            
+            init_apps(self.scrman, self.mqtt)
+    
+            # endless loop for data display
+            while True:
+                self.mqtt.tick()
+                self.scrman.update()
+                    
+                sleep(1)
+        except Exception as e:
+            print(e)
+            logging.exception('Main loop failed')
+            sys.exit(1)
 
         
 if __name__ == '__main__':
-    curses.wrapper(main)
+    datadisp = DataDisp()
+    
+    curses.wrapper(datadisp.run)
