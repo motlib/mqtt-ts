125d19961b32475449ffef73b8ecc1d579910974
Andreas Schroeder <andreas@a-netz.de>
2016-06-05 14:51:28 +0800

    Separate class CmdlApp for command-line tool implementation.

--- src/datadisp.py
+++ src/datadisp.py
@@ -8,61 +8,26 @@ import yaml
 from utils.wdgtfact import WidgetFactory
 from utils.scrman import ScreenManager
 from utils.mqttman import MQTTManager
+from utils.cmdlapp import CmdlApp
 
 
 MQTT_BROKER = '192.168.0.21'
 
-class DataDisp():
+class DataDisp(CmdlApp):
     '''Main class for the MQTT data display application.'''
     
     def setup_args(self):
-        '''Set up the command line parser and parse arguments. 
-
-        The result is stored in self.args.
-
+        '''Override functoin from CmdlApp. Add additional arguments to
+        command-line parser.
         '''
-        
-        parser = ArgumentParser()
 
-        parser.add_argument(
-            '-l', '--logfile',
-            help='Path to the logfile.',
-            default='datadisp.log')
+        CmdlApp.setup_args(self)
 
-        parser.add_argument(
+        self.parser.add_argument(
             '-c', '--cfg',
             help='Path to the YAML config file.',
             default='datadisp.yaml')
-
-        parser.add_argument(
-            '-v', '--verbose',
-            help='Enable verbose logging output.',
-            action='store_true');
-
-        self.args = parser.parse_args()
     
-
-    def setup_logging(self):
-        '''Set up the logging framework.
-
-        Logfile and loglevel are set based on the command line args
-        `logfile` and `verbose`.
-
-        '''
-        
-        if self.args.verbose:
-            level = logging.DEBUG
-        else:
-            level = logging.WARNING
-
-        logging.basicConfig(
-            filename=self.args.logfile,
-            filemode='a',
-            level=level,
-            format='%(asctime)s %(levelname)s: %(message)s')
-
-        logging.info('Starting up')
-
         
     def load_config(self):
         '''Load the configuration file.
@@ -71,17 +36,19 @@ class DataDisp():
 
         '''
         
-        cfgfile = self.args.cfg
+        msg = "Reading config file '{0}'."
+        logging.debug(msg.format(self.args.cfg))
 
         try:
-            with open(cfgfile, 'r') as f:
+            with open(self.args.cfg, 'r') as f:
                 self.cfg = yaml.load(f)
         except:
             msg = "Failed to load config file '{0}'."
-            logging.exception(msg.format(cfgfile))
+            logging.error(msg.format(self.args.cfg))
+            sys.exit(1)
 
             
-    def run(self, stdscr):
+    def curses_main(self, stdscr):
         try:
             self.setup_args()
             
@@ -107,8 +74,14 @@ class DataDisp():
             logging.exception('Main loop failed')
             sys.exit(1)
 
+
+    def main_fct(self):
+        '''Main function to be called by CmdlApp base class.'''
+
+        curses.wrapper(self.curses_main)
+
         
 if __name__ == '__main__':
-    datadisp = DataDisp()
+    DataDisp().run()
     
-    curses.wrapper(datadisp.run)
+
---
+++ src/utils/cmdlapp.py
@@ -0,0 +1,60 @@
+'''Base-class for simple command-line tools.'''
+
+__author__ = 'Andreas <andreas@a-netz.de>'
+
+
+import logging
+from argparse import ArgumentParser
+
+
+class CmdlApp():
+    def __init__(self):
+        pass
+
+    def setup_args(self):
+        self.parser = ArgumentParser()
+
+        self.parser.add_argument(
+            '-v', '--verbose',
+            help='Enable verbose logging output.',
+            action='store_true');
+
+        self.parser.add_argument(
+            '-l', '--logfile',
+            help='Log output to logfile.',
+            default=None)
+
+    
+    def setup_logging(self):
+        '''Set up the logging framework.'''
+
+        if self.args.verbose:
+            level = logging.DEBUG
+            levelname = 'debug'
+        else:
+            level = logging.WARNING
+            levelname = 'warning'
+        
+        lcfg = {
+            'level': level,
+            'format': '%(asctime)s %(levelname)s: %(message)s',
+            }
+
+        if self.args.logfile != None:
+            lcfg['filename'] = self.args.logfile
+
+        logging.basicConfig(**lcfg)
+
+        msg = "Logging system initialized to level '{0}'."
+        logging.debug(msg.format(levelname))
+
+        
+    def run(self):
+        self.setup_args()
+        self.args = self.parser.parse_args()
+
+        self.setup_logging()
+
+
+        self.main_fct()
+
