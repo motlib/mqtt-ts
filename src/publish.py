9c70bd374a53b81e01cc954c2a5c1603c6342319
Andreas Schroeder <andreas@a-netz.de>
2016-06-09 10:29:03 +0800

    Renamed config files to *.template. Removed old python based configuration from publish.py

--- src/publish.py
+++ src/publish.py
@@ -6,49 +6,54 @@ import paho.mqtt.publish as mqtt_pub
 import time
 import os
 
-from sensorconfig import config, get_sensors
 from utils.cmdlapp import CmdlApp
-import sensors
 
 class MqttPublish(CmdlApp):
     def __init__(self):
+        # we are a cmd-line tool with config file
         CmdlApp.__init__(self)
-
         self.cmdlapp_config(has_cfgfile=True)
 
 
     def get_sensor_class(self, name):
+        '''Instanciate the sensor classes based on the config.'''
 
-        mod_name = '.'.join(name.split('.')[0:-1])
+        name_parts = name.split('.')
 
-        np = name.split('.')
-        p = __import__(mod_name)
-        for n in np[1:]:
-            p = getattr(p, n)
+        mod_name = '.'.join(name_parts[0:-1])
+
+        item = __import__(mod_name)
+        for name in name_parts[1:]:
+            item = getattr(item, name)
         
-        return p
+        return item
 
 
     def create_sensors(self):
+        '''Create sensor classes according to the configuration.'''
+
         sensors = []
 
-        for sname,scfg in self.cfg['sensors'].items():
-            cls = self.get_sensor_class(scfg['sensor_class'])
-            
+        for sname, scfg in self.cfg['sensors'].items():
             # add sensor name to config structure
             scfg['sensor_name'] = sname
 
-            inst = cls(scfg)
+            msg = "Instanciate sensor class '{sensor_class}' for sensor " \
+                "'{sensor_name}'."
+            logging.info(msg.format(**scfg))
 
-            sensors.append(inst)
+            scls = self.get_sensor_class(scfg['sensor_class'])
+            sinst = scls(scfg)
+
+            sensors.append(sinst)
 
         return sensors
 
 
     def get_mqtt_path(self, evt):
         data = {
-            'prefix': config['mqtt_topic_prefix'],
-            'node': config['node_name'],
+            'prefix': self.cfg['mqtt']['topic_prefix'],
+            'node': self.cfg['mqtt']['node_name'],
             'sensor': evt.getSensorName(),
             'quantity': evt.getQuantity(),
             }
@@ -86,7 +91,7 @@ class MqttPublish(CmdlApp):
                 mqtt_pub.single(
                     topic=self.get_mqtt_path(evt),
                     payload=evt.toJSON(),
-                    hostname=config['mqtt_broker'])
+                    hostname=self.cfg['mqtt']['broker'])
             except:
                 logging.exception('Publish of MQTT value failed.')
         
@@ -95,19 +100,13 @@ class MqttPublish(CmdlApp):
         '''Set up the sensors and publish cyclic updates of the sensor
         values to the MQTT broker.'''
 
-        logging.info('Setting up sensors')
-        #self.sensors = get_sensors()
-
         self.sensors = self.create_sensors()
-
-        logging.debug('Sensors initialized.')
-
 
         while True:
             evts = self.sample_sensors()
             self.publish_events(evts)
 
-            #time.sleep(config['sample_interval'])
+            #TODO: better scheduling, best per sensor
             time.sleep(5)
 
 
